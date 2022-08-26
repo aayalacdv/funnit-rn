42ec4256cc3b676bf1fb61912f716f82f6e55c62
aayalacdv <aayalacdv2000@gamil.com>
2022-08-26 12:07:43 +0200

    added validation to signup screen

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react'
 import { AuthService } from '../helpers/auth/auth.service'
 import { auth } from '../firebase/config'
-import { Center, Heading, KeyboardAvoidingView } from 'native-base'
+import { Center, Heading, KeyboardAvoidingView, Text} from 'native-base'
 import InputField from './login-screen/InputField'
 import CustomButton from './login-screen/CustomButton'
 import { Platform } from 'react-native'
+import { Controller, useForm } from 'react-hook-form'
 
 const authService = AuthService(auth)
 
-const SignUpScreen: React.FC<{navigation : any}> = ({navigation}) => {
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [confirmPassword, setConfirmPassword] = useState<string>('')
+const SignUpScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
+    const { control, handleSubmit, getValues, formState: { errors } } = useForm({
+        defaultValues: {
+            email: '',
+            password: '', 
+            confirmPassword: ''
+        }
+    });
 
-    const handleCreateAccount= async () => {
-        authService.signUpWithPasswordAndEmail(email, password)
-        .then((newUser) => {
-            navigation.navigate('Login')
-        })
-        .catch((e) => alert('check credentials'))
+    const handleCreateAccount = async (data : any) => {
+        authService.signUpWithPasswordAndEmail(data.email, data.password)
+            .then((newUser) => {
+                navigation.navigate('Login')
+            })
+            .catch((e) => alert('check credentials'))
     }
 
     return (
@@ -27,10 +32,83 @@ const SignUpScreen: React.FC<{navigation : any}> = ({navigation}) => {
         >
             <Center h='100%'>
                 <Heading marginY={2}> Create an account</Heading>
-                {/* <InputField placeHolder='Email' value={email} onChangeText={setEmail} type={'text'} />
-                <InputField placeHolder='Password' value={password} onChangeText={setPassword} type={'password'} />
-                <InputField placeHolder='Repeat password' value={confirmPassword} onChangeText={setConfirmPassword} type={'password'} /> */}
-                <CustomButton text='Create Account' onPress={() => handleCreateAccount()} />
+                <Controller
+                    name='email'
+                    control={control}
+                    rules={{
+                        required: {
+                            value: true,
+                            message: 'email is required'
+                        },
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "invalid email address"
+                        }
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <InputField
+                            placeHolder='email'
+                            type='text'
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                    )}
+                />
+                {errors.email && <Text>{errors.email.message}</Text>}
+
+                <Controller
+                    name='password'
+                    control={control}
+                    rules={{
+                        required: {
+                            value: true,
+                            message: 'password is required'
+                        },
+                        minLength: {
+                            value: 6,
+                            message: 'minimun lenght is 6'
+                        }
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <InputField
+                            placeHolder='password'
+                            type='password'
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                    )}
+                />
+                {errors.password && <Text>{errors.password.message}</Text>}
+
+                <Controller
+                    name='confirmPassword'
+                    control={control}
+                    rules={{
+                        required: {
+                            value: true,
+                            message: 'password is required'
+                        },
+                        validate: {
+                            value: (value) => value === getValues().password
+                        }
+
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <InputField
+                            placeHolder='password'
+                            type='password'
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                    )}
+                />
+                {errors.confirmPassword && <Text>Passwords must match</Text>}
+
+                <CustomButton text='Create Account' onPress={handleSubmit(handleCreateAccount)} />
+
                 <CustomButton text='Login' onPress={() => navigation.navigate('Login')} />
             </Center>
 
