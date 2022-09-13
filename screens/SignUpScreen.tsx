8e2737707d480b094b36689c7373ab4d7cf5d439
aayalacdv <aayalacdv2000@gamil.com>
2022-09-13 17:18:09 +0200

    styled registration form

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
 import { AuthService } from '../helpers/auth/auth.service'
 import { auth } from '../firebase/config'
-import { Center, Heading, KeyboardAvoidingView, Text} from 'native-base'
+import { Button, Center, Container, Heading, KeyboardAvoidingView, Text } from 'native-base'
 import InputField from './login-screen/InputField'
 import CustomButton from './login-screen/CustomButton'
 import { Platform } from 'react-native'
 import { Controller, useForm } from 'react-hook-form'
+import Logo from '../components/Logo'
 
 const authService = AuthService(auth)
+const BG_COLOR = 'rgb(255,77,77)'
 
 const SignUpScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
     const { control, handleSubmit, getValues, formState: { errors } } = useForm({
         defaultValues: {
             email: '',
-            password: '', 
+            password: '',
             confirmPassword: ''
         }
     });
 
-    const handleCreateAccount = async (data : any) => {
+    const handleCreateAccount = async (data: any) => {
         authService.signUpWithPasswordAndEmail(data.email, data.password)
             .then((newUser) => {
                 navigation.navigate('Login')
@@ -27,91 +29,115 @@ const SignUpScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
     }
 
     return (
-            <Center h='100%'>
         <KeyboardAvoidingView
+            backgroundColor={BG_COLOR} w='full' h='full'
             behavior={Platform.OS === "ios" ? "padding" : "height"}
+            alignItems='center'
+
         >
-                <Heading marginY={2}> Create an account</Heading>
-                <Controller
-                    name='email'
-                    control={control}
-                    rules={{
-                        required: {
-                            value: true,
-                            message: 'email is required'
-                        },
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                            message: "invalid email address"
-                        }
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <InputField
-                            placeHolder='email'
-                            type='text'
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                />
-                {errors.email && <Text>{errors.email.message}</Text>}
+            <Container w={'full'} marginY={5} alignItems='center'>
+                <Logo />
+            </Container>
 
-                <Controller
-                    name='password'
-                    control={control}
-                    rules={{
-                        required: {
-                            value: true,
-                            message: 'password is required'
-                        },
-                        minLength: {
-                            value: 6,
-                            message: 'minimun lenght is 6'
-                        }
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <InputField
-                            placeHolder='password'
-                            type='password'
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                />
-                {errors.password && <Text>{errors.password.message}</Text>}
+            <Controller
+                name='email'
+                control={control}
+                rules={{
+                    required: {
+                        value: true,
+                        message: 'email is required'
+                    },
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "invalid email address"
+                    }
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <InputField
+                        placeHolder='email'
+                        type='text'
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+            />
+            {errors.email && <Text>{errors.email.message}</Text>}
 
-                <Controller
-                    name='confirmPassword'
-                    control={control}
-                    rules={{
-                        required: {
-                            value: true,
-                            message: 'password is required'
-                        },
-                        validate: {
-                            value: (value) => value === getValues().password
-                        }
+            <Controller
+                name='password'
+                control={control}
+                rules={{
+                    required: {
+                        value: true,
+                        message: 'password is required'
+                    },
+                    minLength: {
+                        value: 6,
+                        message: 'minimun lenght is 6'
+                    }
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <InputField
+                        placeHolder='password'
+                        type='password'
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+            />
+            {errors.password && <Text>{errors.password.message}</Text>}
 
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <InputField
-                            placeHolder='password'
-                            type='password'
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                />
-                {errors.confirmPassword && <Text>Passwords must match</Text>}
+            <Controller
+                name='confirmPassword'
+                control={control}
+                rules={{
+                    required: {
+                        value: true,
+                        message: 'password is required'
+                    },
+                    validate: {
+                        value: (value) => value === getValues().password
+                    }
 
-                <CustomButton text='Create Account' onPress={handleSubmit(handleCreateAccount)} />
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <InputField
 
-                <CustomButton text='Login' onPress={() => navigation.navigate('Login')} />
+                        placeHolder='password'
+                        type='password'
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+            />
+            {errors.confirmPassword && <Text>Passwords must match</Text>}
+
+            <Button
+                backgroundColor={'white'}
+                shadow={9}
+                w={'70%'}
+                marginY={4}
+
+                onPress={handleSubmit(handleCreateAccount)}>
+                <Text>
+                    Registrarse
+                </Text>
+            </Button>
+
+            <Button
+                backgroundColor={'white'}
+                shadow={9}
+                w={'70%'}
+
+                onPress={() => navigation.navigate('Login')} >
+                <Text>
+                    Iniciar sesión
+                </Text>
+            </Button>
         </KeyboardAvoidingView>
-            </Center>
 
     )
 }
